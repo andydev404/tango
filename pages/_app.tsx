4b419c7791c61f5044c3ba99ec9f7e6090b48d50
Andy <andysantana0106@gmail.com>
2023-01-07 11:43:13 -0400

    feat: add chara-ui package

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
-import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import { colors } from '@styles/theme';
+
+const theme = extendTheme({
+  colors
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />{' '}
+    </ChakraProvider>
+  );
 }
